62a2e866fe58c18296b2ff016bbdff9859dcea43
carlonavone <cnavone@mail.csuchico.edu>
2020-03-30 20:33:20 -0700

    Week 9 Instructions

--- TopTenList.cpp
+++ TopTenList.cpp
@@ -7,15 +7,16 @@ using std::string;
 
 TopTenList::TopTenList()
 {
-
+  _list.resize(10)
 }
 
 void TopTenList::set_at(int index, Hyperlink link)
 {
-
+  _list[index - 1] = link;
 }
-
+//Get item at certain index
 Hyperlink TopTenList::get(int index)
 {
-
+  int index = index - 1;
+  return _list.at(index);
 }
